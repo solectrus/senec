9eaf84f54c57ff3400098526588eb5b6a2e8ca93
Georg Ledermann <georg@ledermann.dev>
2022-11-13 09:08:54 +0100

    Value: Refactoring, add tests

--- lib/senec/value.rb
+++ lib/senec/value.rb
@@ -4,37 +4,35 @@ module Senec
       @data = data
     end
 
-    def to_f
-      decoded_value
-    end
-
-    def to_i
-      decoded_value.round
-    end
-
-    private
-
-    def decoded_value
+    def decoded
       parts  = @data.split('_')
       prefix = parts[0]
       value  = parts[1]
 
       case prefix
       when 'fl'
-        hex2float(value)
-      when 'i3', 'u1', 'u3'
-        hex2int(value)
+        decoded_float(value)
+      when 'i3', 'u1', 'u3', 'u6', 'u8'
+        decoded_int(value)
+      when 'st'
+        value
       # TODO: There are some more prefixes to handle
       else
-        raise ArgumentError, "#{@data} cannot be decoded!"
+        raise ArgumentError, "#{@data} has unknown prefix!"
       end
     end
 
-    def hex2float(hex)
+    alias to_i decoded
+    alias to_f decoded
+    alias to_s decoded
+
+    private
+
+    def decoded_float(hex)
       ["0x#{hex}".to_i(16)].pack('L').unpack1('F').round(1)
     end
 
-    def hex2int(hex)
+    def decoded_int(hex)
       "0x#{hex}".to_i(16)
     end
   end
---
+++ spec/lib/senec/value_spec.rb
@@ -0,0 +1,55 @@
+RSpec.describe Senec::Value do
+  subject(:value) { described_class.new(data) }
+
+  describe '#decoded' do
+    subject(:decoded) { value.decoded }
+
+    context 'when fl' do
+      let(:data) { 'fl_437E7A5F' }
+
+      it { is_expected.to be_a(Float) }
+      it { is_expected.to eq(254.5) }
+    end
+
+    context 'when u1' do
+      let(:data) { 'u1_FFFF' }
+
+      it { is_expected.to be_a(Integer) }
+      it { is_expected.to eq(65_535) }
+    end
+
+    context 'when u3' do
+      let(:data) { 'u3_FFFFFFFF' }
+
+      it { is_expected.to be_a(Integer) }
+      it { is_expected.to eq(4_294_967_295) }
+    end
+
+    context 'when u6' do
+      let(:data) { 'u6_FFFFFFFFFFFFFFFF' }
+
+      it { is_expected.to be_a(Integer) }
+      it { is_expected.to eq(18_446_744_073_709_551_615) }
+    end
+
+    context 'when u8' do
+      let(:data) { 'u8_FF' }
+
+      it { is_expected.to be_a(Integer) }
+      it { is_expected.to eq(255) }
+    end
+
+    context 'when st' do
+      let(:data) { 'st_Test' }
+
+      it { is_expected.to be_a(String) }
+      it { is_expected.to eq('Test') }
+    end
+
+    context 'when unknown' do
+      let(:data) { 'xx_123' }
+
+      it { expect { decoded }.to raise_error(ArgumentError) }
+    end
+  end
+end
