200df35c066385058737b8347cd7099065f4c5f3
Georg Ledermann <georg@ledermann.dev>
2023-02-04 11:12:24 +0100

    Cleanup specs

--- spec/lib/senec/request_spec.rb
+++ spec/lib/senec/request_spec.rb
@@ -5,67 +5,67 @@ RSpec.describe Senec::Request do
     let(:host) { 'senec' }
 
     describe '#house_power' do
-      subject { super().house_power }
+      subject { request.house_power }
 
       it { is_expected.to eq(511.6) }
     end
 
     describe '#inverter_power' do
-      subject { super().inverter_power }
+      subject { request.inverter_power }
 
       it { is_expected.to eq(2338.7) }
     end
 
     describe '#bat_power' do
-      subject { super().bat_power }
+      subject { request.bat_power }
 
       it { is_expected.to eq(1792.9) }
     end
 
     describe '#bat_fuel_charge' do
-      subject { super().bat_fuel_charge }
+      subject { request.bat_fuel_charge }
 
       it { is_expected.to eq(51.5) }
     end
 
     describe '#bat_charge_current' do
-      subject { super().bat_charge_current }
+      subject { request.bat_charge_current }
 
       it { is_expected.to eq(34.0) }
     end
 
     describe '#bat_voltage' do
-      subject { super().bat_voltage }
+      subject { request.bat_voltage }
 
       it { is_expected.to eq(52.7) }
     end
 
     describe '#grid_power' do
-      subject { super().grid_power }
+      subject { request.grid_power }
 
       it { is_expected.to eq(-34.3) }
     end
 
     describe '#wallbox_charge_power' do
-      subject { super().wallbox_charge_power }
+      subject { request.wallbox_charge_power }
 
       it { is_expected.to eq([0.0, 0.0, 0.0, 0.0]) }
     end
 
     describe '#case_temp' do
-      subject { super().case_temp }
+      subject { request.case_temp }
 
       it { is_expected.to eq(34.6) }
     end
 
     describe '#current_state' do
-      subject { super().current_state }
+      subject { request.current_state }
 
       it { is_expected.to eq('CHARGE') }
     end
 
     describe '#measure_time' do
-      subject { Time.at(super().measure_time) }
+      subject { Time.at(request.measure_time) }
 
       it { is_expected.to eq(Time.parse('2022-11-13 09:30:05.000000000 +0100')) }
     end
